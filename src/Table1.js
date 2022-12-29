2a1e5f8946c9fc875b823c7d79545b10676b0d80
vivek-smartenovations <vivek.ap@smartrnovations.com>
2022-12-29 18:21:04 +0530

    first commit

--- src/Table1.js
+++ src/Table1.js
@@ -3,74 +3,68 @@ import {
   Typography,
   TableContainer,
   TableHead,
-   
   TextField,
   Box,
+  IconButton,
 } from "@mui/material";
 import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
 import TableCell from "@mui/material/TableCell";
 import Table from "@mui/material/Table";
 import TableRow from "@mui/material/TableRow";
 import TableBody from "@mui/material/TableBody";
 import Card from "@mui/material/Card";
-// import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { LineAxisOutlined } from "@mui/icons-material";
 
 function Table1() {
   const [projectDetails, setProjectDetails] = React.useState([]);
-  const [projectStatus, setProjectStatus] = React.useState("");
+  const [name, setName] = React.useState("name");
 
   console.log(projectDetails);
-  const [options, setOptions] = React.useState({
-    project_status: { choices: [] },
-  });
 
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  React.useEffect(() => {
- 
-  }, []);
-
-   
- 
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 80,
-      },
-    },
+  const function1 = () => {
+    setName("change");
   };
 
- 
+  React.useEffect(() => {
+    fetch(
+      `https://api.jikan.moe/v4/characters?page=1&limit=5&order_by=favorites&sort=desc`
+    )
+      .then((results) => results.json())
+      .then((data) => {
+        setProjectDetails(data.data);
+      });
+  }, []);
+
   return (
-    <Card sx={{ margin:"10px" }}>
+    <Card sx={{ margin: "10px" }}>
       <TableContainer component={Paper}>
-      <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-     
-    </Box>
-        <div
-          className="float"
-          style={{ display: "flex", height: "fitContent" }}
-        >
+        <Box>
+          <TextField
+            sx={{ width: "20%", margin: "8px" }}
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              ),
+            }}
+          >
+            {" "}
+          </TextField>
+        </Box>
+        <div className="float">
           <Typography
-          sx={{ flex: '1 1 100%', padding: "20px" }}
-            variant="h5"
+            sx={{ flex: "1 1 100%", padding: "20px" }}
+            variant="h6"
             id="tableTitle"
             padding="5px"
           >
-            Projects waiting for approval
+            Total 50 matching anime charectores found
           </Typography>
         </div>
         <Table>
@@ -83,29 +77,23 @@ function Table1() {
               }}
               style={{ backgroundColor: "#e0e0e0" }}
             >
-              <TableCell sx={{ fontSize: "14px" }}>Project Name</TableCell>
-              <TableCell sx={{ fontSize: "14px" }}>Project ID</TableCell>
-              <TableCell sx={{ fontSize: "14px" }}>Project Scop</TableCell>
-              <TableCell sx={{ fontSize: "14px" }}>
-                HardWare Requirement
-              </TableCell>
-              <TableCell sx={{ fontSize: "14px" }}>Resource type</TableCell>
-              
-              <TableCell sx={{ fontSize: "14px" }}>Actions</TableCell>
+              <TableCell>Anime img</TableCell>
+              <TableCell>Anime Name</TableCell>
+
+              <TableCell>Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-           
-              <TableRow>
-               
-                <TableCell sx={{ fontSize: "16px" }}>
-                  jbdfb
+            {projectDetails.map((data) => (
+              <TableRow key={data.mal_id}>
+                <TableCell>{data.name}</TableCell>
+                <TableCell>{name}</TableCell>
+                <TableCell>
+                  {" "}
+                  <ArrowForwardIcon onClick={function1} />
                 </TableCell>
-               
-                
               </TableRow>
-             
-           
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
